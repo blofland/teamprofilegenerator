a0f66efeac2ac792430bc24a8676033c84a9412f
Nate <boedi15@gmail.com>
2021-11-29 09:57:18 -0700

    Corrected static method error for Employee properties

--- index.js
+++ index.js
@@ -1,65 +1,69 @@
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
+// const Employee = require('./lib/Employee');
+// const Engineer = require('./lib/Engineer');
+// const Intern = require('./lib/Intern');
+// const Manager = require('./lib/Manager');
 
-function addManager() {
-    inquirer.prompt([
-        {
-            type: 'text',
-            name: 'office number',
-            message: 'What is your office number?'
-        },
+// function addManager() {
+//     inquirer.prompt([
+//         {
+//             type: 'text',
+//             name: 'office number',
+//             message: 'What is your office number?'
+//         },
 
-        {
-            type: 'list',
-            name: 'role',
-            message: 'Who would you like to add to your team?',
-            choices: ['Engineer', 'Intern', 'Finished']
-        }
+//         {
+//             type: 'list',
+//             name: 'role',
+//             message: 'Who would you like to add to your team?',
+//             choices: ['Engineer', 'Intern', 'Finished']
+//         }
 
-    ])
-}
+//     ])
+// }
 
-function addIntern() {
-    inquirer.prompt([
-        {
-            type: 'text',
-            name: 'school',
-            message: 'What school do you go to?'
-        }
-    ])
-}
+// function addIntern() {
+//     inquirer.prompt([
+//         {
+//             type: 'text',
+//             name: 'school',
+//             message: 'What school do you go to?'
+//         }
+//     ])
+// }
 
-function addEngineer() {
-    inquirer.prompt([
-        {
-            type: 'text',
-            name: 'github',
-            message: 'What is your github username?'
-        }
-    ])
-}
+// function addEngineer() {
+//     inquirer.prompt([
+//         {
+//             type: 'text',
+//             name: 'github',
+//             message: 'What is your github username?'
+//         }
+//     ])
+// }
 
 
 
-function addEmployee() {
-    inquirer.prompt([
-  {
-        type: 'text',
-        name: 'name',
-        message: 'What is your name?'
-    },
-   {
-        type: 'text',
-        name: 'email',
-        message: 'What is your email?'
-    },
-    {
-        type: 'number',
-        name: 'id',
-        message: 'What is your ID number?'
-    },
-])}
+// function addEmployee() {
+//     inquirer.prompt([
+//   {
+//         type: 'text',
+//         name: 'name',
+//         message: 'What is your name?'
+//     },
+//    {
+//         type: 'text',
+//         name: 'email',
+//         message: 'What is your email?'
+//     },
+//     {
+//         type: 'number',
+//         name: 'id',
+//         message: 'What is your ID number?'
+//     },
+// ])}
+
+const Team = require("./lib/Team")
+const team = new Team()
+team.prompt()

cae0575ce6405fa36b2c0918270b64502c4f1dc6
unknown <boedi15@gmail.com>
2021-11-28 17:01:33 -0700

    added inquirer to employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,11 @@
+const { it } = require('@jest/globals');
+const { describe } = require('yargs');
 const Employee = require('../lib/Employee');
 
-test('create a new employee', () => {
-    const employee = new Employee('Phil');
+describe("Employees should have basic properties",()=>{
+    it("should have a name", ()=>{
+        const employee = new Employee('Phil');
 
-    expect(employee.name).toBe('Phil');
+        expect(employee.name).toBe('Phil');
+    })
 })

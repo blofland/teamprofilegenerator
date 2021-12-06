e957c649558fe12adc87a072a122499bdfbec696
blofland <84938746+blofland@users.noreply.github.com>
2021-12-06 00:05:05 -0700

    Updated employee unit test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,15 @@
-const { it } = require('@jest/globals');
-const { describe } = require('yargs');
 const Employee = require('../lib/Employee');
 
 describe("Employees should have basic properties",()=>{
+    const params = {name: 'Phil', id:1, email: "email@email.com"}
+    const employee = new Employee(params)
     it("should have a name", ()=>{
-        const employee = new Employee('Phil');
-
-        expect(employee.name).toBe('Phil');
+        expect(employee.name).toBe(params.name);
+    })
+    it("should have an email", () => {
+        expect(employee.email).toBe(params.email)
+    })
+    it("should have an id", () => {
+        expect(employee.id).toBe(params.id)
     })
 })
